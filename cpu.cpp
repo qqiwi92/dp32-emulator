c55e1628d95aadd70d3cc8d4abe0930494d25ee9
qqiwi92 <mit.levkin@vk.com>
2026-03-01 12:07:55 +0300

    add addition and substraction (quick too)

--- cpu.cpp
+++ cpu.cpp
@@ -2,10 +2,11 @@
 #include "bus.hpp"
 #include <cstdint>
 
-CPU::CPU(BUS* b) : bus(b), pc(0), v(false), n(false), z(false) {
-    for (int i = 0; i < 256; i++) {
-        regs[i] = 0; 
-    }
+CPU::CPU(BUS* b) : bus(b), pc(0), v(false), n(false), z(false)
+{
+  for (int i = 0; i < 256; i++) {
+    regs[i] = 0;
+  }
 }
 void CPU::step()
 {
@@ -22,7 +23,7 @@ void CPU::step()
   case 0x00:
     execute_add(r3, r1, r2);
     break;
-    // todo: opps
+    // TODO: THE REST OF THE OPERATIONS
   }
 }
 void CPU::update_cc(uint32_t result, bool overflow_happened)
@@ -32,12 +33,8 @@ void CPU::update_cc(uint32_t result, bool overflow_happened)
   v = overflow_happened;
 }
 
-uint32_t CPU::get_reg(int index) const {
-    return regs[index];
-}
-void CPU::set_reg(uint8_t index, uint32_t value) {
-    regs[index] = value;
-}
+uint32_t CPU::get_reg(int index) const { return regs[index]; }
+void CPU::set_reg(uint8_t index, uint32_t value) { regs[index] = value; }
 
 void CPU::execute_add(uint8_t r3, uint8_t r1, uint8_t r2)
 {
@@ -45,13 +42,68 @@ void CPU::execute_add(uint8_t r3, uint8_t r1, uint8_t r2)
   int32_t b = static_cast< int32_t >(regs[r2]);
 
   int32_t res = a + b;
-  bool ovf  = false;
-  
-  if (b > 0 && a > (INT32_MAX - b)) ovf = true;
-  else if (b < 0 && a < (INT32_MIN -b)) ovf  = true;
-  
-  regs[r3] = static_cast<uint32_t>(res);
-  
+  bool ovf = false;
+
+  if (b > 0 && a > (INT32_MAX - b))
+    ovf = true;
+  else if (b < 0 && a < (INT32_MIN - b))
+    ovf = true;
+
+  regs[r3] = static_cast< uint32_t >(res);
+
   update_cc(regs[r3], ovf);
-  
+}
+
+void CPU::execute_add_quick(uint8_t r3, uint8_t r1, uint8_t r2)
+{
+  int32_t a = static_cast< int32_t >(regs[r1]);
+  int32_t b = static_cast< int32_t >(r2);
+
+  int32_t res = a + b;
+  bool ovf = false;
+
+  if (b > 0 && a > (INT32_MAX - b))
+    ovf = true;
+  else if (b < 0 && a < (INT32_MIN - b))
+    ovf = true;
+
+  regs[r3] = static_cast< uint32_t >(res);
+
+  update_cc(regs[r3], ovf);
+}
+
+void CPU::execute_substruct(uint8_t r3, uint8_t r1, uint8_t r2)
+{
+  int32_t a = static_cast< int32_t >(regs[r1]);
+  int32_t b = static_cast< int32_t >(regs[r2]);
+
+  int32_t res = a - b;
+  bool ovf = false;
+
+  if (b > 0 && a < (INT32_MIN + b))
+    ovf = true;
+  else if (b < 0 && a > (INT32_MAX + b))
+    ovf = true;
+
+  regs[r3] = static_cast< uint32_t >(res);
+
+  update_cc(regs[r3], ovf);
+}
+
+void CPU::execute_substruct_quick(uint8_t r3, uint8_t r1, uint8_t r2)
+{
+  int32_t a = static_cast< int32_t >(regs[r1]);
+  int32_t b = static_cast< int32_t >(r2);
+
+  int32_t res = a - b;
+  bool ovf = false;
+
+  if (b > 0 && a < (INT32_MIN + b))
+    ovf = true;
+  else if (b < 0 && a > (INT32_MAX + b))
+    ovf = true;
+
+  regs[r3] = static_cast< uint32_t >(res);
+
+  update_cc(regs[r3], ovf);
 }
--- cpu.hpp
+++ cpu.hpp
@@ -12,10 +12,11 @@ public:
   CPU(BUS* b);
   void step();
   uint32_t get_reg(int index) const;
-  void set_reg(uint8_t index, uint32_t value) ;
+  void set_reg(uint8_t index, uint32_t value);
   bool get_z() const { return z; }
   bool get_n() const { return n; }
   bool get_v() const { return v; }
+
 private:
   BUS* bus;
   uint32_t regs[256]; // РОН
@@ -30,17 +31,16 @@ private:
   void execute_substruct(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_multiply(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_divide(uint8_t r3, uint8_t r1, uint8_t r2);
-  
+
   void execute_add_quick(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_substruct_quick(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_multiply_quick(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_divide_quick(uint8_t r3, uint8_t r1, uint8_t r2);
-  
+
   void execute_and(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_or(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_xor(uint8_t r3, uint8_t r1, uint8_t r2);
   void execute_mask(uint8_t r3, uint8_t r1, uint8_t r2);
-  
 };
 
 #endif
