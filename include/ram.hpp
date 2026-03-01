e2f435286282cd62319e25ec4aa5dad2f088bc07
Dima <mit.levkin@vk.com>
2026-03-01 18:06:54 +0300

    more specific errors

--- include/ram.hpp
+++ include/ram.hpp
@@ -11,20 +11,34 @@ public:
     for (int i = 0; i < 65536; i++)
       memory[i] = 0;
   }
-  uint32_t read(uint32_t address) { 
-      if (address >= 65536) {
-          std::cerr << "SEGFAULT: Attempt to read address " << address << std::endl;
-          return 0; 
+  uint32_t read(uint32_t address)
+  {
+    if (address >= 65536) {
+      int32_t int_repr = static_cast< int32_t >(address);
+      if (int_repr < 0) {
+        std::cerr
+            << "You've likely overflowed by passing negative displacement\n";
       }
-      return memory[address]; 
+      std::cerr << "SEGFAULT: Attempt to read address " << address << std::endl;
+      return 0;
+    }
+    return memory[address];
   }
-  void write(uint32_t address, uint32_t value) { 
-      if (address >= 65536) {
-          std::cerr << "SEGFAULT: Attempt to write address " << address << std::endl;
-          return;
+  void write(uint32_t address, uint32_t value)
+  {
+    if (address >= 65536) {
+      int32_t int_repr = static_cast< int32_t >(address);
+      if (int_repr < 0) {
+        std::cerr
+            << "You've likely overflowed by passing negative displacement\n";
       }
-      memory[address] = value; 
+      std::cerr << "SEGFAULT: Attempt to write address " << address
+                << std::endl;
+      return;
+    }
+    memory[address] = value;
   }
+
 private:
   uint32_t memory[65536];
 };
